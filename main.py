910ea4b258f5aa5a5908ef151d576f289783db88
Phil <79200375+phil-beep@users.noreply.github.com>
2022-10-20 14:33:06 +0200

    api for receiving drawings

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+import schemas
+import tensorflow as tf
 
 app = FastAPI()
 
-@app.get("/")
-def first_api():
-    return {"test" : "hello world"}
+@app.get("/identify_drawing/")
+def identify_drawing(drawing: schemas.Drawing):
+    return tf_analysis(drawing);
+
+def tf_analysis(drawing: schemas.Drawing):
+
+    #todo: connect with tensorflow api
+    return(tf.__version__)
+
