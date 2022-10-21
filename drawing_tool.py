6788feeec6848d7b5768429f66efe7d32263e506
Phil <79200375+phil-beep@users.noreply.github.com>
2022-10-21 13:39:10 +0200

    drawing tool for debug purposes

---
+++ drawing_tool.py
@@ -0,0 +1,35 @@
+import schemas
+from PIL import Image, ImageDraw
+
+canvas_size = 500
+img = Image.new("RGB", (canvas_size, canvas_size), "white")
+draw = ImageDraw.Draw(img)
+
+def draw_image(drawing_container: schemas.Drawing):
+
+    # zip x,y coordinates
+    strokes = (zip(stroke[0], stroke[1]) for stroke in drawing_container.drawing)
+
+    #change function later depending on how drawing works in frontend
+    render_stop_while_drawing(strokes)
+
+    img.show()
+
+def render_stop_while_drawing(strokes):
+
+    for outer_container in strokes:
+        extracted_coordinates = []
+        for inner_container in outer_container:
+            extracted_coordinates += inner_container
+            draw.line(extracted_coordinates, fill="black", width = 2)
+
+
+def render_dont_stop_while_drawing(strokes):
+    
+    extracted_coordinates = []
+    for outer_container in strokes:
+        for inner_container in outer_container:
+            extracted_coordinates += inner_container
+            draw.line(extracted_coordinates, fill="black", width = 2)
+    
+
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 import schemas
 import tensorflow as tf
+import drawing_tool
 
 app = FastAPI()
 
 @app.get("/identify_drawing/")
 def identify_drawing(drawing: schemas.Drawing):
+    drawing_tool.draw_image(drawing)
     return tf_analysis(drawing);
 
 def tf_analysis(drawing: schemas.Drawing):
