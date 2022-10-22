4e1d0c56c0ccf88d56e9f0c67898cd1970fa569a
Phil <79200375+phil-beep@users.noreply.github.com>
2022-10-22 08:06:16 +0200

    refactoring

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 from fastapi import FastAPI
 import schemas
-import tensorflow as tf
 import drawing_tool
+from PIL import Image
+from keras_preps import analysis
 
 app = FastAPI()
 
 @app.get("/identify_drawing/")
 def identify_drawing(drawing: schemas.Drawing):
-    drawing_tool.draw_image(drawing)
-    return tf_analysis(drawing);
-
-def tf_analysis(drawing: schemas.Drawing):
-
-    #todo: connect with tensorflow api
-    return(tf.__version__)
+    img = drawing_tool.draw_image(drawing)
+    return analysis(img)
 
