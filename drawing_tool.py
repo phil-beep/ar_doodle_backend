fa51f0514f2d3cdcf24ec1ea9240f822806b2d7e
Phil <79200375+phil-beep@users.noreply.github.com>
2022-10-28 10:26:56 +0200

    refactoring

--- drawing_tool.py
+++ drawing_tool.py
@@ -1,22 +1,24 @@
 import schemas
 from PIL import Image, ImageDraw
-
+from globals import image_size
 canvas_size = 256
-img = Image.new("RGB", (canvas_size, canvas_size), "white")
-draw = ImageDraw.Draw(img)
 
-def draw_image(drawing_container: schemas.Drawing):
+
+def draw_image_from_coordinates(drawing_container: schemas.Drawing):
+    img = Image.new("RGB", (canvas_size, canvas_size), "white")
+    draw = ImageDraw.Draw(img)
 
     # zip x,y coordinates
     strokes = (zip(stroke[0], stroke[1]) for stroke in drawing_container.drawing)
 
     #change function later depending on how drawing works in frontend
-    render_stop_while_drawing(strokes)
+    render_stop_while_drawing(strokes, draw)
     #img.show()
-    resized = img.resize(size=(28, 28))
-    return resized;
+    img = img.resize(size=image_size)
+    img.show()
+    return img
 
-def render_stop_while_drawing(strokes):
+def render_stop_while_drawing(strokes, draw):
 
     for outer_container in strokes:
         extracted_coordinates = []
@@ -25,7 +27,7 @@ def render_stop_while_drawing(strokes):
             draw.line(extracted_coordinates, fill="black", width = 2)
 
 
-def render_dont_stop_while_drawing(strokes):
+def render_dont_stop_while_drawing(strokes, draw):
     
     extracted_coordinates = []
     for outer_container in strokes:
