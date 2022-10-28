6a93615ebb0759003590b46f069fcfeca10d2033
Phil <79200375+phil-beep@users.noreply.github.com>
2022-10-28 10:31:49 +0200

    refactoring

--- keras_preps.py
+++ keras_preps.py
@@ -3,20 +3,30 @@ from tensorflow.keras.utils import img_to_array
 import tensorflow as tf
 import numpy as np
 import labels
+import drawing_tool
+from globals import input_size
 
-def analysis(img: Image):
+def load_model():
     f = open('./models/current_version.txt')
     model_used = f.read()
     model = tf.keras.models.load_model('./models/' + model_used )
-    model.predict(tf.zeros([1, 28, 28, 1]))
-    
+    model.predict(tf.zeros([1, input_size, input_size , 1]))
+    return model
+
+def preprocess_drawing(coordinates):
+    img = drawing_tool.draw_image_from_coordinates(coordinates)
     tensor_img = img_to_array(img)
     tensor_img = tf.expand_dims(tensor_img, 0)
-    tensor_img /= 255.
+    tensor_img /= 255
+    return tensor_img
 
+def analysis(tensor_img, model):
     predictions = model.predict(tensor_img)
-    classes = np.argmax(predictions, axis=1)
-
-    result = labels.labels[classes[0]]
-
+    print(predictions)
+    classes = np.argmax(predictions[0])
+    print(classes)
+    result = labels.labels[classes]
+    print(result)
     return(result)
+
+
