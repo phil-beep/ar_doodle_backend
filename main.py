bc6c7f89f1c6b257b9d4a54c2977cc6931998e12
Phil <79200375+phil-beep@users.noreply.github.com>
2022-10-28 10:26:30 +0200

    refactoring, debug method

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from fastapi import FastAPI
 import schemas
-import drawing_tool
 from PIL import Image
-from keras_preps import analysis
+from keras_preps import *
 
 app = FastAPI()
 
 @app.get("/identify_drawing/")
-def identify_drawing(drawing: schemas.Drawing):
-    img = drawing_tool.draw_image(drawing)
-    return analysis(img)
+def identify_drawing(drawing_coordinates: schemas.Drawing):
+    model = load_model()
+    img = preprocess_drawing(drawing_coordinates)
+    return analysis(img, model)
 
+
+#debug route to load any image from the dataset
+@app.get("/{animal}/{image_name}")
+def identify_picture(animal, image_name):
+    model = load_model()
+    
+    img = Image.open("dataset/" + animal + "/" + image_name + ".png")
+    img.show()
+    tensor_img = img_to_array(img)
+    tensor_img = tf.expand_dims(tensor_img, 0)
+    tensor_img /= 255
+    return analysis(tensor_img, model)
