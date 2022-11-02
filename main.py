aa89ab0df7b7feab8eaa2c64b90edebdb3195bbb
Phil <79200375+phil-beep@users.noreply.github.com>
2022-11-02 18:41:28 +0100

    refactoring, load model only once, fixed routes

--- main.py
+++ main.py
@@ -3,36 +3,34 @@ from fastapi import FastAPI
 import schemas
 from PIL import Image
 from keras_preps import *
-from globals import image_size
+
 app = FastAPI()
+model = load_model()
 
 @app.get("/identify_drawing/")
 def identify_drawing(drawing_coordinates: schemas.Drawing):
-    model = load_model()
-    img = preprocess_drawing(drawing_coordinates)
-    return analysis(img, model)
+    img = drawing_tool.draw_image_from_coordinates(drawing_coordinates)
+    tensor_img = preprocess_drawing(img)
+    return analysis(tensor_img, model)
 
 
 #debug route to load any image from the dataset
-@app.get("/{animal}/{image_name}")
+@app.get("/training_data/{animal}/{image_name}")
 def identify_picture(animal, image_name):
-    model = load_model()
-    
     img = Image.open("dataset/" + animal + "/" + image_name + ".png").convert('L')
-    img.show()
-    tensor_img = img_to_array(img)
-    tensor_img = tf.expand_dims(tensor_img, 0)
-    tensor_img /= 255
+    tensor_img = preprocess_drawing(img)
     return analysis(tensor_img, model)
+
+
+@app.get("/test_data/{image_name}")
+def identify_picture(image_name):
+    img = Image.open("test_data/" + image_name + ".png").convert('L')
+    tensor_img = preprocess_drawing(img)
+    return analysis(tensor_img, model)
+
 
 @app.post("/file_path/")
 def image_from_file_path(file_path: schemas.File_path):
-    
     img = Image.open(file_path.imageLocation).convert('L')
-    img = img.resize(size = image_size)
-    model = load_model()
-    img.show()
-    tensor_img = img_to_array(img)
-    tensor_img = tf.expand_dims(tensor_img, 0)
-    tensor_img /= 255
+    tensor_img = preprocess_drawing(img)
     return analysis(tensor_img, model)
