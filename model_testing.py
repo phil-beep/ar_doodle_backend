e97e044996b9c0f0d626f8f521e6140affa646c0
Phil <79200375+phil-beep@users.noreply.github.com>
2022-11-08 23:41:46 +0100

    init multi recognition test

---
+++ model_testing.py
@@ -0,0 +1,31 @@
+from keras_preps import load_model, preprocess_drawing, analysis
+from main import identify_picture
+from PIL import Image
+
+def testing():
+    model = load_model()
+
+    #add file names from test_data folder here
+    images = [
+        #"crab1",
+        #"fish1"
+    ]
+
+    result = ""
+
+    for image in images:
+        prediction = testing_with(image, model)
+        result += "\n" + image + "->" + prediction
+    
+    print(result)
+    
+
+
+
+def testing_with(image_name, model):
+    img = Image.open("test_data/" + image_name + ".png")
+    tensor_img = preprocess_drawing(img)
+    return analysis(tensor_img, model)
+
+
+testing()
