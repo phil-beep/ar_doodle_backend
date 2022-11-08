9b47a10ca0d7c5eb34b1602d3d7ff11df175170e
Phil <79200375+phil-beep@users.noreply.github.com>
2022-11-08 23:35:43 +0100

    less magic numbers

--- globals.py
+++ globals.py
@@ -1,8 +1,10 @@
+from labels import labels
+
 input_size = 28
 image_size = (input_size, input_size)
 batch_size = 32
 input_shape = (input_size, input_size, 1)
-n_classes = 5
+n_classes = len(labels)
 epochs = 5
 max_drawings = 1000
 validation_split = 0.2
