9edcc3205aebcdd13eadb293824d8cc7b2f3f2fd
Phil <79200375+phil-beep@users.noreply.github.com>
2022-11-11 09:48:11 +0100

    updated params

--- globals.py
+++ globals.py
@@ -1,10 +1,11 @@
 from labels import labels
 
-input_size = 28
+input_size = 32
 image_size = (input_size, input_size)
-batch_size = 32
+batch_size = 64
 input_shape = (input_size, input_size, 1)
 n_classes = len(labels)
-epochs = 5
-max_drawings = 1000
+epochs = 20
+max_drawings = 50000
 validation_split = 0.2
+canvas_size = 128
