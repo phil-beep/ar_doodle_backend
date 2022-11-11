807a0a938f927906bae7e19a56e58dc9b1784915
Phil <79200375+phil-beep@users.noreply.github.com>
2022-11-11 10:21:57 +0100

    fit drawing rendering to unity coordinates

--- drawing_tool.py
+++ drawing_tool.py
@@ -1,38 +1,34 @@
 import schemas
 from PIL import Image, ImageDraw
-from globals import image_size
-canvas_size = 256
-
+from globals import canvas_size
 
 def draw_image_from_coordinates(drawing_container: schemas.Drawing):
-    img = Image.new("RGB", (canvas_size, canvas_size), "white")
-    draw = ImageDraw.Draw(img)
-
-    # zip x,y coordinates
-    strokes = (zip(stroke[0], stroke[1]) for stroke in drawing_container.drawing)
-
-    #change function later depending on how drawing works in frontend
-    render_stop_while_drawing(strokes, draw)
-    #img.show()
-    img = img.resize(size=image_size)
-    img.show()
+    canvas = Image.new("RGB", (canvas_size, canvas_size), "white")
+    coords = (zip(stroke[0], stroke[1]) for stroke in drawing_container.drawing)
+    img = render_unity_pixels(coords, canvas)
     return img
 
-def render_stop_while_drawing(strokes, draw):
+#debug only, will be removed, use unity coordinates
+def render_quickdraw_strokes(coords, canvas):
+    x = ImageDraw.Draw(canvas)
 
-    for outer_container in strokes:
+    for outer_container in coords:
         extracted_coordinates = []
         for inner_container in outer_container:
             extracted_coordinates += inner_container
-            draw.line(extracted_coordinates, fill="black", width = 2)
+            x.line(extracted_coordinates, fill="black", width = 2)
+    
+    return canvas
 
 
-def render_dont_stop_while_drawing(strokes, draw):
+def render_unity_pixels(coords, canvas):
     
     extracted_coordinates = []
-    for outer_container in strokes:
+    for outer_container in coords:
         for inner_container in outer_container:
             extracted_coordinates += inner_container
-            draw.line(extracted_coordinates, fill="black", width = 2)
+            canvas.putpixel((inner_container), (0,0,0))
+    
+    return canvas
     
 
