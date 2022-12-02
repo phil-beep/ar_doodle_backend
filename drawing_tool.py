486629855ab87d1bfa3da5646659e79c4a9d50db
Phil <79200375+phil-beep@users.noreply.github.com>
2022-12-02 22:27:44 +0100

    removed debug functions

--- drawing_tool.py
+++ drawing_tool.py
@@ -8,27 +8,12 @@ def draw_image_from_coordinates(drawing_container: schemas.Drawing):
     img = render_unity_pixels(coords, canvas)
     return img
 
-#debug only, will be removed, use unity coordinates
-def render_quickdraw_strokes(coords, canvas):
-    x = ImageDraw.Draw(canvas)
-
-    for outer_container in coords:
-        extracted_coordinates = []
-        for inner_container in outer_container:
-            extracted_coordinates += inner_container
-            x.line(extracted_coordinates, fill="black", width = 2)
-    
-    return canvas
-
-
 def render_unity_pixels(coords, canvas):
-    
     extracted_coordinates = []
     for outer_container in coords:
         for inner_container in outer_container:
             extracted_coordinates += inner_container
             canvas.putpixel((inner_container), (0,0,0))
-    
     return canvas
     
 
--- main.py
+++ main.py
@@ -7,32 +7,11 @@ from drawing_tool import draw_image_from_coordinates
 app = FastAPI()
 model = load_model()
 
-@app.post("/identify_drawing/")
+@app.post("/identify_drawing/" )
 def identify_drawing(drawing_coordinates: schemas.Drawing):
     img = draw_image_from_coordinates(drawing_coordinates)
     tensor_img = preprocess_drawing(img)
     return analysis(tensor_img, model)
 
 
-#debug route to load any image from the dataset
-@app.post("/training_data/{animal}/{image_name}")
-def identify_picture(animal, image_name):
-    img = Image.open("dataset/" + animal + "/" + image_name + ".png")
-    tensor_img = preprocess_drawing(img)
-    return analysis(tensor_img, model)
 
-
-@app.post("/test_data/{image_name}")
-def identify_picture(image_name):
-    img = Image.open("test_data/" + image_name + ".png")
-    background = Image.new('RGBA', img.size, (255,255,255))
-    img = Image.alpha_composite(background, img)
-    tensor_img = preprocess_drawing(img)
-    return analysis(tensor_img, model)
-
-
-@app.post("/file_path/")
-def image_from_file_path(file_path: schemas.File_path):
-    img = Image.open(file_path.imageLocation)
-    tensor_img = preprocess_drawing(img)
-    return analysis(tensor_img, model)
