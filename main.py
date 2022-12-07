58eaf8163e8b426d8de92a51d1587288ba7ffbc0
Phil <79200375+phil-beep@users.noreply.github.com>
2022-12-07 16:31:21 +0100

    remove api docs pages

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from PIL import Image
 from keras_preps import load_model, preprocess_drawing, analysis
 from drawing_tool import draw_image_from_coordinates
 
-app = FastAPI()
+app = FastAPI(openapi_url=None)
 model = load_model()
 
-@app.post("/identify_drawing/" )
-def identify_drawing(drawing_coordinates: schemas.Drawing):
+@app.post("/identify_drawing/")
+def identify_drawing(drawing_coordinates: schemas.Drawing, ):
     img = draw_image_from_coordinates(drawing_coordinates)
     tensor_img = preprocess_drawing(img)
     return analysis(tensor_img, model)
